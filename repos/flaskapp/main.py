df0c432e9ad064dc7c3af2f39d19f12c9202e242
lwndp <55749141+lwndp@users.noreply.github.com>
2025-02-20 19:46:53 +0100

    end of lab-2

--- repos/flaskapp/main.py
+++ repos/flaskapp/main.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Home Page"
+    return render_template("home.html")
+
+@app.route("/hello/<city>")
+def hello(city):
+    return f"<h1>Hello, {city}!</h1>"
+
+@app.route("/about")
+def about():
+    return render_template("about.html") 
+
+if __name__ == '__main__':
+    app.run(debug=True)
